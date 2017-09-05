97bd8543c26871fe935ca7ca210f3453f679f23f
maguas01 <maguas01@gmail.com>
2017-09-05 01:02:03 -0400

    problem set 0

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//$("#main").append("Manuel Esteban Aguas");
+//var thoughts = "I am manny and I am awesome" ;
+//$("#main").append(thoughts.replace("awesome", "fun"));
+
+var name = "Manuel Esteban Aguas";
+var role = "Software Engineer";
+
+var formattedName =  HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
